f27a879c5004742936147332b9786fedb6b6cf98
saahilmahato <mahatosaahil4@gmail.com>
2026-04-14 12:00:00 +0545

    feat: base class methods

--- src/main/java/org/saahil/SwiftCache.java
+++ src/main/java/org/saahil/SwiftCache.java
@@ -1,31 +1,36 @@
 package org.saahil;
 
+import org.saahil.read.ReadStrategy;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SwiftCache<K, V> {
     private final ConcurrentHashMap<K, CacheEntry<V>> cache = new ConcurrentHashMap<>();
     private final CacheStats stats;
+    private final ReadStrategy<K, V> readStrategy;
 
-    public SwiftCache() {
+    public SwiftCache(ReadStrategy<K, V> readStrategy) {
         this.stats = new CacheStats();
+        this.readStrategy = readStrategy;
+    }
+
+    public CacheStats getStats() {
+        return this.stats;
     }
 
     public void printStats() {
         System.out.println(this.stats);
     }
 
-    private CacheEntry<V> getEntry(K key) {
-        this.stats.recordHit();
-        return cache.get(key);
+    public V get(K key) {
+        return readStrategy.get(key, this);
     }
 
-    private void putEntry(K key, V value, long ttlNanos) {
-        cache.put(key, new CacheEntry<V>(value, ttlNanos));
-        this.stats.getPuts();
+    public CacheEntry<V> getEntry(K key) {
+        return this.cache.get(key);
     }
 
-    private void removeEntry(K key) {
-        cache.remove(key);
-        this.stats.recordEviction();
+    public void removeEntry(K key) {
+        this.cache.remove(key);
     }
 }
