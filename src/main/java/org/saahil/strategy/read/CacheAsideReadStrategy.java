22a4dd75b4a0fe0ad84d90fd522818197279f618
saahilmahato <mahatosaahil4@gmail.com>
2026-05-15 17:10:00 +0545

    feat: cache aside strategy

--- src/main/java/org/saahil/strategy/read/CacheAsideReadStrategy.java
+++ src/main/java/org/saahil/strategy/read/CacheAsideReadStrategy.java
@@ -3,8 +3,7 @@ package org.saahil.strategy.read;
 import org.saahil.CacheEntry;
 import org.saahil.SwiftCache;
 
-public class CacheAsideReadStrategy<K, V>
-        implements ReadStrategy<K, V> {
+public class CacheAsideReadStrategy<K, V> implements ReadStrategy<K, V> {
 
     @Override
     public V read(K key, SwiftCache<K, V> cache) {
@@ -12,18 +11,13 @@ public class CacheAsideReadStrategy<K, V>
         CacheEntry<V> entry = cache.getInternalEntry(key);
 
         if (entry == null) {
-
             cache.getStats().recordMiss();
-
             return null;
         }
 
         if (entry.isExpired()) {
-
             cache.removeInternal(key);
-
             cache.getStats().recordMiss();
-
             return null;
         }
 
