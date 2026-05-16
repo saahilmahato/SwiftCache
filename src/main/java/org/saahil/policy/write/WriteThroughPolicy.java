c6cdedadaca8849d7d16f470a458311807b8683e
saahilmahato <mahatosaahil4@gmail.com>
2026-05-16 17:20:00 +0545

    feat: write through policy

--- src/main/java/org/saahil/policy/write/WriteThroughPolicy.java
+++ src/main/java/org/saahil/policy/write/WriteThroughPolicy.java
@@ -1,4 +1,20 @@
 package org.saahil.policy.write;
 
-public class WriteThroughPolicy {
+import org.saahil.SwiftCache;
+
+public class WriteThroughPolicy<K, V> implements WritePolicy<K, V> {
+
+    @Override
+    public void write(
+            K key,
+            V value,
+            long ttlNanos,
+            SwiftCache<K, V> cache
+    ) {
+
+        // simulate database write
+        System.out.println("Writing to DB: " + key);
+
+        cache.putInternal(key, value, ttlNanos);
+    }
 }
